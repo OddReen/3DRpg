1c6cdb8d4e328df65e580eb2f322970550facf51
OddReen <leonardovneves2002@gmail.com>
2026-03-05 18:52:00 +0000

    Initial commit

---
+++ Assets/Scripts/DataLibrary.cs
@@ -0,0 +1,12 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public static class DataLibrary
+{
+    public enum VoxelType
+    {
+        Dirt,
+        Stone,
+        Wood
+    }
+}
---
+++ Assets/Scripts/MeshCreation.cs
@@ -0,0 +1,58 @@
+using NUnit.Framework.Internal;
+using System;
+using UnityEditor;
+using UnityEngine;
+using static LNeves.MeshCreation;
+using static UnityEditor.Searcher.SearcherWindow.Alignment;
+
+namespace LNeves
+{
+    [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+    public class MeshCreation : MonoBehaviour
+    {
+        private MeshFilter meshFilter;
+        private Mesh mesh;
+
+        public MeshWrap meshWrap;
+
+        [Serializable]
+        public struct MeshWrap
+        {
+            public Vector3[] vertices;
+            public int[] triangles;
+            public Vector2[] uvs;
+        }
+
+        public void InitMesh()
+        {
+            mesh?.Clear();
+            mesh = new Mesh();
+            meshFilter = GetComponent<MeshFilter>();
+            meshFilter.mesh = mesh;
+        }
+
+        public void UpdateMesh(MeshWrap InMeshWrap)
+        {
+            if (InMeshWrap.triangles.Length % 3.0f == 0)
+            {
+                mesh.Clear();
+                mesh.vertices = InMeshWrap.vertices;
+                mesh.triangles = InMeshWrap.triangles;
+                mesh.uv = InMeshWrap.uvs;
+                mesh.RecalculateNormals();
+            }
+        }
+
+        void OnDrawGizmos()
+        {
+            // foreach(Vector3 Vertice in Voxel.vertices)
+            // {
+            //     Handles.Label(Vertice, Vertice.ToString());
+            // }
+            // for (int i = 0; i < meshWrap.vertices.Length; i++)
+            // {
+            //     Handles.Label(meshWrap.vertices[i], i.ToString());
+            // }
+        }
+    }
+}
